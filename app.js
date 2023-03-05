86733b9792da546afe7cd2b7bb51bcbc7eab92ff
BibekRajPaudel <paudelrajbibek@gmail.com>
2023-03-05 20:16:24 +0545

    fixesss

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const app = express();
 cors = require("cors");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const Router = require("./routes/apiRoutes");
-const scrape =require("./routes/gateHoursRoute")
-
-
-
+const scrape = require("./routes/gateHoursRoute");
 
 app.use(cors());
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.get("/", (req, res, next) => {
   res.send("Server started !!!!!!!!!!");
@@ -19,6 +16,5 @@ app.get("/", (req, res, next) => {
 
 app.use("/api/v1/gateHours", Router);
 app.use("/api/v1/crud", scrape);
-
 
 module.exports = app;
